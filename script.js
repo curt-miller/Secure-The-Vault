0dbe4cce6c99adafd886f979fdc138eb59bbff94
Curt Miller <millercw3@gmail.com>
2024-11-04 12:07:01 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// - The below "message" defines the text in the alert
+
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// Below are three mathmatical operations that result in the combination - one operation per number 
+
+const number1 = 2 * 5;
+const number2 = 25 + 15;
+const number3 = 117 / 3;
+
+// The "alert" code geerates an alert popup in the browser
+
+alert(`${message} to the vault is ${number1}-${number2}-${number3}`);
